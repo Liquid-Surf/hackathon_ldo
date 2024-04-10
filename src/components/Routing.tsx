30939c17f49e60bb0f86b26b8f2e6cbdb7fd7f09
thhck <thhck@proton.me>
2024-04-10 17:36:07 +0200

    fix typos and spelling

--- src/components/Routing.tsx
+++ src/components/Routing.tsx
@@ -30,82 +30,80 @@ export const Routing: FunctionComponent = () => {
     }
   }, [getResource, session.webId]);
 
-  const welcome =  
-  <>
-    <h3>
-    	Welcome to my project for the <a href="https://solidhack.org">Solid Mini-hackathon 2024</a>
-    </h3>
+  const welcome =
+    <>
+      <h3>
+        Welcome to my project for the <a href="https://solidhack.org">Solid Mini-hackathon 2024</a>
+      </h3>
 
-    <p>
-    	On this website you can create and view the tutorial that you have hosted on your Pod. 
-    	Therefor, you need a pod to use this app, you can create one here: <a href="https://pod.liquid.surf">pod.liquid.surf</a>
-    </p>
-    <h3>Usage</h3>
-    <p>
-    <ol>
-      <li>Login with you pod</li>
-      <li>Create a tutorial on the "Create" page</li>
-      <li>View the list of tutorial on the Home page or "My tutorals" page</li>
-      <li>Click on the tutorial preview to see the full tutorial</li>
-      <li>You can delete a tutorial clicking the "Delete" button at the end of a tutorial page</li>
-    </ol>
-    	
-    </p>
-
+      <p>
+        On this website, you can create and view the tutorial that you have hosted on your Pod.
+        Therefore, you need a pod to use this app, you can create one here: <a href="https://pod.liquid.surf">pod.liquid.surf</a>
+      </p>
+      <h3>Usage</h3>
+      <p>
+        <ol>
+          <li>Login with your pod</li>
+          <li>Create a tutorial on the "Create" page</li>
+          <li>View the list of tutorials on the Home page or "My tutorials" page</li>
+          <li>Click on the tutorial preview to see the full tutorial</li>
+          <li>You can delete a tutorial by clicking the "Delete" button at the end of a tutorial page</li>
+        </ol>
+      </p>
     </>
-    
+
 
   return (
     <Router>
       <Header />
-        <Routes>
-      {session.isLoggedIn &&  mainContainer ? (
-        <>
+      <Routes>
+        {session.isLoggedIn && mainContainer ? (
+          <>
+            <Route
+              path="/"
+              element={
+                <Container>
+                  <ListTuts mainContainer={mainContainer} />
+                </Container>
+              }
+            />
+            <Route
+              path="/create"
+              element={
+                <Container>
+                  <Create mainContainer={mainContainer} />
+                </Container>
+              }
+            />
+            <Route
+              path="/myprojects"
+              element={
+                <Container>
+                  <ListTuts mainContainer={mainContainer} />
+                </Container>
+              }
+            />
+            <Route
+              path="/project/:tutUri"
+              element={
+                <Container>
+                  <TutorialRoute />
+                </Container>
+              }
+            />
+          </>
+        ) :
+
           <Route
             path="/"
             element={
               <Container>
-                <ListTuts mainContainer={mainContainer} />
+                {welcome}
               </Container>
             }
           />
-          <Route
-            path="/create"
-            element={
-              <Container>
-                <Create mainContainer={mainContainer} />
-              </Container>
-            }
-          />
-          <Route
-            path="/myprojects"
-            element={
-              <Container>
-                <ListTuts mainContainer={mainContainer} />
-              </Container>
-            }
-          />
-          <Route
-            path="/project/:tutUri"
-            element={
-              <Container>
-                <TutorialRoute />
-              </Container>
-            }
-          />
-        </>
-      ) : 
-
-        <Route
-          path="/"
-          element={
-            <Container>
-              {welcome}
-            </Container>
-          }
-        />
-       }
-        </Routes>
+        }
+      </Routes>
     </Router>
   );
 };
